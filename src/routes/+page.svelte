4da1a023c9183d97981445c02692a8c129b9f4e9
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-11-10 16:07:39 -0800

    chores: clean up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
-	import SuperDebug from 'sveltekit-superforms';
 	import Form from './form.svelte';
-	import { Button } from '$lib/components/ui/button';
 
 	let { data } = $props();
-	let secret = $state('');
 </script>
 
 <div class="container mt-8">
