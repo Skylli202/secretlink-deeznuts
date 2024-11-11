aa60aec6fa88362d9300e4c7214043a6ddfb838c
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-11-10 16:08:49 -0800

    wip: nav bar?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,8 @@
 	import '../app.css';
 </script>
 
+<nav class="mb-8 mt-4 flex items-stretch justify-center space-x-4 lg:space-x-6">
+	<p>Secret-DeezNuts</p>
+	<a href="/about">About</a>
+</nav>
 <slot />
