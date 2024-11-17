f87e4834c20ac74c7b8620af3fafd65210555f58
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-11-17 13:03:49 -0800

    feat: nice error page on /secret/[id] endpoint

---
+++ src/routes/secrets/[id]/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import SuperDebug from 'sveltekit-superforms';
+</script>
+
+<div class="container">
+	{#if $page.status === 404}
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Secret not found</Card.Title>
+				<Card.Description
+					>Your secret is probably already deleted (most likely view count reached or expire date).</Card.Description
+				>
+			</Card.Header>
+			<Card.Content>
+				<Button variant="link">
+					<a href="/"> Go back to the home page </a>
+				</Button>
+			</Card.Content>
+		</Card.Root>
+	{:else}
+		<div></div>
+	{/if}
+</div>
+
