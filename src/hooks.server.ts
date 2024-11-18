33c3ee36525728d93ee2888f21be52c1bca6a039
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-11-17 20:30:50 -0800

    feat: better error handling in hooks with PocketBase connection

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,17 @@
 import { env } from '$env/dynamic/private'
-import PocketBase from 'pocketbase'
+import { error } from '@sveltejs/kit'
+import PocketBase, { ClientResponseError } from 'pocketbase'
 
 export async function handle({ event, resolve }) {
-  event.locals.pb = new PocketBase('http://127.0.0.1:8090')
-  await event.locals.pb.admins.authWithPassword(env.PB_ADMIN_EMAIL, env.PB_ADMIN_PASSWORD)
+  event.locals.pb = new PocketBase(env.PB_URL)
+  try {
+
+    const adminAuth = await event.locals.pb.admins.authWithPassword(env.PB_ADMIN_EMAIL, env.PB_ADMIN_PASSWORD)
+  } catch (e) {
+    if (e instanceof ClientResponseError) {
+      return error(503, 'Service Unavailable: PocketBase unreachable, or unable to authenticate to the service.')
+    }
+  }
   const response = await resolve(event)
   return response
 }
