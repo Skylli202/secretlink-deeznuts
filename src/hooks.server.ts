2a38da283c8ce01b050352c9913337a8575247f4
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-11-17 21:34:36 -0800

    feat: improve log and error handling at hook level

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,17 +4,14 @@ import PocketBase, { ClientResponseError } from 'pocketbase'
 
 export async function handle({ event, resolve }) {
   try {
-    console.log('AAAAAAAAAAAAAAAA')
     event.locals.pb = new PocketBase(env.PB_URL)
-    console.log('BBBBBBBBBBBBBBBB')
     const adminAuth = await event.locals.pb.admins.authWithPassword(env.PB_ADMIN_EMAIL, env.PB_ADMIN_PASSWORD)
-    console.log('CCCCCCCCCCCCCCCC')
   } catch (e) {
-    console.log('AAAAAAAAAAAAAAAA')
     if (e instanceof ClientResponseError) {
       console.error(`Failed to connect to PocketBase at URL: ${env.PB_URL} with login: ${env.PB_ADMIN_EMAIL}.`)
-      return error(503, 'Service Unavailable: DB unreachable or cannot authenticate to.')
+      return error(503, 'Service Unavailable: PocketBase error.')
     }
+    return error(503, 'Service Unavailable: Unknown error.')
   }
   const response = await resolve(event)
   return response
