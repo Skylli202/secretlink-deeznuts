8522a6436ecec0e1988c0cac3e586316215d98a9
E. GOUINGUENET <elouangouinguenet@gmail.com>
2024-11-17 17:55:55 -0800

    wip: about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root>
+	<Card.Content>
+		<Card.Title tag="h1">About</Card.Title>
+		<Card.Description>This page has not been build yet. Come back later :)</Card.Description>
+	</Card.Content>
+</Card.Root>
