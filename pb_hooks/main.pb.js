d88ef0a35fe15ad40218051dc2a2837336da509e
egouinguenet <elouangouinguenet@gmail.com>
2025-03-09 21:03:21 -0700

    deps: upgraded pocketbase hooks to work with 0.23.0 JSVM

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -1,17 +1,20 @@
 /// <reference path="../pb_data/types.d.ts" />
 
 onRecordViewRequest((e) => {
-  // Increase view count
-  e.record.set('viewCount', e.record.getInt("viewCount") + 1)
-  $app.dao().saveRecord(e.record)
-  // console.log(`Increasing view count for record "${e.record.getId()}".`)
-  $app.logger().info(`Increasing view count for record "${e.record.getId()}".`)
+  try {
+    // Increase view count
+    e.record.set('viewCount', e.record.getInt("viewCount") + 1)
+    $app.save(e.record)
+    $app.logger().info(`Increased view count for record "${e.record.id}".`)
 
-  // If the max view count is reach, delete the record.
-  if (e.record.getInt("viewCount") === e.record.getInt("maxViewCount")) {
-    $app.dao().deleteRecord(e.record)
-    // console.log(`Maximum view count exceeded for record "${e.record.getId()}".`)
-    $app.logger().info(`Maximum view count exceeded for record "${e.record.getId()}".`)
+    // If the max view count is reach, delete the record.
+    if (e.record.getInt("viewCount") >= e.record.getInt("maxViewCount")) {
+      $app.delete(e.record)
+      $app.logger().info(`Maximum view count exceeded for record "${e.record.getId()}".`)
+    }
+    e.next()
+  } catch (e) {
+    console.log(e)
   }
 }, "secrets")
 
